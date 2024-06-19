fa2837af21e67b28923279d07f762e484360e3bf
Dieter Blocher <dieter.blocher@mitutoyo-ctl.de>
2024-06-19 20:26:01 +0000

    add video

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import StreamingResponse
+import os
 
 app = FastAPI()
 
+VIDEO_FILE_PATH = "./assets/file_example_MP4_1920_18MG.mp4"
 
 @app.get("/")
 def read_root():
@@ -9,3 +12,14 @@ def read_root():
         "Hello": "World",
         "How Cool": "Is This?! :-D"
     }
+
+@app.get("/video")
+async def get_video():
+    if not os.path.exists(VIDEO_FILE_PATH):
+        raise HTTPException(status_code=404, detail="Video file not found")
+
+    def iterfile():
+        with open(VIDEO_FILE_PATH, mode="rb") as file_like:
+            yield from file_like
+
+    return StreamingResponse(iterfile(), media_type="video/mp4")
